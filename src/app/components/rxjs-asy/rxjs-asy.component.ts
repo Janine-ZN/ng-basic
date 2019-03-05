71502a5581e6346706cabbc11dcaeb889b05ead6
Janine-ZN <Jainie_z@163.com>
2019-03-05 22:36:49 +0800

    promise & observable

--- src/app/components/rxjs-asy/rxjs-asy.component.ts
+++ src/app/components/rxjs-asy/rxjs-asy.component.ts
@@ -15,8 +15,27 @@ export class RxjsAsyComponent implements OnInit {
     const data = this.request.getData();
     console.log(data);
 
-    const callbackdata = this.request.getCallbackData();
-    console.log(callbackdata);
+    // const callbackdata = this.request.getCallbackData();
+    const callbackdata = this.request.getCallbackData((res) => {
+      console.log(res); // 输出张三
+      /*
+        // 将此方法传到 getCallbackData方法中，然后执行该方法
+        // 执行时，正好把 username 传过来了
+        (res) => {console.log(res); // 输出张三}
+      */
+    });
+
+
+    const promiseData = this.request.getPromiseData();
+    promiseData.then((result) => {
+      console.log(result);
+    });
+
+    const observeData = this.request.getRxjsData();
+    observeData.subscribe((result) => {
+        console.log(result);
+    });
+
   }
 
 }
--- src/app/services/request.service.ts
+++ src/app/services/request.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -7,15 +8,43 @@ export class RequestService {
 
   constructor() { }
 
-  // 同步
+  // 同步方法
   getData() {
     return 'this is service data';
   }
 
-  getCallbackData() {
+  /**
+  * 异步方法
+  * 在调用 getCallbackData() 的时候传过来一个 cb 函数
+  */
+  getCallbackData(cb) {
     setTimeout(() => {
       const username = '张三';
-      return username;
+      // return username;   // undefined
+      // 执行cb方法
+      cb(username);
+
     }, 1000);
   }
+
+  // ES6 -- Promise
+  getPromiseData() {
+    return new Promise((resovle) => {
+      setTimeout(() => {
+        const username = '张三--promise';
+        resovle(username);
+      }, 1500);
+    });
+  }
+
+  // rxjs 的观察者模式
+  getRxjsData() {
+    return new Observable((observer) => {
+      setTimeout(() => {
+        const username = '张三-observe';
+        observer.next(username);
+      }, 2000);
+    });
+  }
+
 }
