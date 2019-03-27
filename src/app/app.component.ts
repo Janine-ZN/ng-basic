f488d9373c6ac8181b3117479640c285c079f15b
Janine-ZN <Janine_z@163.com>
2019-03-27 18:04:37 +0800

    observer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NoDataPipe } from './core/no-data.pipe';
+import { Producer } from 'src/app/observer/Producer';
 
 interface File {
   name: string;
@@ -20,9 +21,12 @@ export class AppComponent implements OnInit {
   mapped: File[];
   title = 'ng-basic';
   constructor(
-    private onDataPipe: NoDataPipe
+    private onDataPipe: NoDataPipe,
+    private producer: Producer
   ) { }
+
   ngOnInit(): void {
+    // this.producer.addListener();
     this.files = [
       { name: 'logo.svg', size: 2120109, type: 'image/svg' },
       { name: 'banner.jpg', size: 18029, type: 'image/jpg' },
@@ -35,5 +39,15 @@ export class AppComponent implements OnInit {
         size: this.onDataPipe.transform(file.size)
       };
     });
+    console.log(`新数据：${this.mapped}`);
   }
+
+  listener1(message) {
+    console.log(message + 'from listener1');
+  }
+
+  listener2(message) {
+    console.log(message + 'from listener1');
+  }
+
 }
