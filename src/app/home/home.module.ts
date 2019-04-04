ade0a5eafefb1c90d5e80f6d891fd89c683ef98f
Janine-ZN <Janine_z@163.com>
2019-04-04 14:51:48 +0800

    home菜单

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
-
+import { NgZorroAntdModule } from 'ng-zorro-antd';
 @NgModule({
   declarations: [HomeComponent],
   imports: [
+    NgZorroAntdModule,
     CommonModule
   ]
 })
