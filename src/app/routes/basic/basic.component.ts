39071471ea027b46ae03d53f65634b0a9699049a
Janine-ZN <Jainie_z@163.com>
2019-05-01 11:58:52 +0800

    添加和删除数组元素

--- src/app/routes/basic/basic.component.ts
+++ src/app/routes/basic/basic.component.ts
@@ -7,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BasicComponent implements OnInit {
   imgUrl: any = 'assets/imgs/yichan05.jpg';
+  num = 12.5;
+  emp = {
+    ename: 'lilei',
+    gender: 1,
+    salary: 10000,
+    birthdate: 1501234567890,
+    projectList: ['项目1', '项目2', '项目3'],
+  };
+  showInfo() {
+    console.log('调用了showInfo()');
+    return '123';
+  }
   changeImg() {
     const num: any = Math.floor(Math.random() * 5 + 1);
     console.log('num===>', num);
--- src/app/routes/directive/page/directive-basic/directive-basic.component.ts
+++ src/app/routes/directive/page/directive-basic/directive-basic.component.ts
@@ -8,6 +8,23 @@ import { Component, OnInit } from '@angular/core';
 export class DirectiveBasicComponent implements OnInit {
   private title: String = '我是一个div';
   private msg = '<h1>我是innerHTML</h1>';
+
+  scores = [89, 91, 71];
+  elist = [
+    { eid: 101, ename: 'dingding', gender: 1 },
+    { eid: 102, ename: 'tuangtuang', gender: 0 },
+    { eid: 103, ename: 'biangbiang', gender: 1 },
+  ];
+  addScore() {
+    const num = Math.floor(Math.random() * 100);
+    this.scores.push(num);
+  }
+
+  delete(index) {
+    this.elist.splice(index, 1);
+  }
+
+
   constructor() { }
 
   ngOnInit() {
