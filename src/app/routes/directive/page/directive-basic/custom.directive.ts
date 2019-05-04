ea4208a8d531c02d7fc3a4232cfeef15347a8f64
Janine-ZN <Jainie_z@163.com>
2019-05-04 23:08:45 +0800

    练习自定义指令

--- src/app/routes/directive/page/directive-basic/custom.directive.ts
+++ src/app/routes/directive/page/directive-basic/custom.directive.ts
@@ -8,11 +8,12 @@ export class CustomDirective {
   constructor(el: ElementRef) {
     console.log('自定义指令');
     setInterval(() => {
-      if (!el.nativeElement.style.visibility) {
-        el.nativeElement.style.visibility = 'hidden';
-      } else {
-        el.nativeElement.style.visibility = 'visible';
-      }
+      // 获取当前的显示方式
+      let v = el.nativeElement.style.visibility;
+      console.log(v);
+      // 修改当前的显示方式
+      v = (v === 'hidden' ? 'visible' : 'hidden');
+      el.nativeElement.style.visibility = v;
     }, 1000);
   }
 
